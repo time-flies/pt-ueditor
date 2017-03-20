a13a32a5ae710b2122fb8c1ee1fd6e973a00e357
shirley <jxjx921225@163.com>
2017-03-20 17:28:44 +0800

    给浏览器两边预留空白区域。

--- ueditor.all.js
+++ ueditor.all.js
@@ -6911,7 +6911,8 @@ var fillCharReg = new RegExp(domUtils.fillChar, 'g');
                     //font-family不能呢随便改，在safari下fillchar会有解析问题
                     'body{margin:8px;font-family:sans-serif;font-size:16px;}' +
                     //设置段落间距
-                    'p{margin:5px 0;}</style>' +
+                    // 'p{margin:5px 0;}</style>' +
+                    'p{margin:5px 20px;min-width: 700px; max-width: 1150px}</style>' +
                     (options.iframeCssUrl ? '<link rel=\'stylesheet\' type=\'text/css\' href=\'' + utils.unhtml(options.iframeCssUrl) + '\'/>' : '' ) +
                     (options.initialStyle ? '<style>' + options.initialStyle + '</style>' : '') +
                     '</head>' +
